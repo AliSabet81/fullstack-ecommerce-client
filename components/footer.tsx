7c126e21d3c4d34c3b5d8b6d14e5f34a26338281
AliSabet81 <sabet.farshid1381@gmail.com>
2024-06-12 01:06:51 +0330

    starting routes and implement footer component

---
+++ components/footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">
+          &copy; AliStore, Inc. All right reserved{" "}
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
