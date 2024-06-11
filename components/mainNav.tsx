5bdfa74c2b62ebc47df9a7a3815e3a54852b7319
AliSabet81 <sabet.farshid1381@gmail.com>
2024-06-12 01:25:10 +0330

    starting main nav

---
+++ components/mainNav.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { FC } from "react";
+
+interface MainNavProps {
+  data: any;
+}
+
+const MainNav: FC<MainNavProps> = ({ data }) => {
+  const pathname = usePathname();
+  const routes = data.map((route) => ({
+    href: `/category/${route.id}`,
+    label: route.name,
+    active: pathname === `/category/${route.id}`,
+  }));
+
+  return (
+    <div className="mx-6 flex items-center space-x-4 lg:space-x-6">
+      {routes.map((route) => (
+        <Link
+          key={route.href}
+          href={route.href}
+          className={cn(
+            "text-sm font-medium transition-colors hover:text-black",
+            route.active ? "text-black" : "text-neutral-500"
+          )}
+        >
+          {route.label}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default MainNav;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Container from "@/components/ui/container";
+import Link from "next/link";
+import MainNav from "./mainNav";
 
 const Navbar = () => {
   return (
     <div className="border-b">
-      <Container>Navbar</Container>
+      <Container>
+        <div className="relative px-4 sx:px-6 lg:px-8 flex h-16 items-center">
+          <Link href={"/"} className="ml-4 flex lg:ml-0 gap-x-2">
+            <p className="font-bold text-xl">Store</p>
+          </Link>
+          <MainNav data={[]} />
+        </div>
+      </Container>
     </div>
   );
 };
