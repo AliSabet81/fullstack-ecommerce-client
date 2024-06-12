256ee895730f54ed91edcdcdd720e5c6096dd4a2
AliSabet81 <sabet.farshid1381@gmail.com>
2024-06-12 20:54:36 +0330

    starting product page

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -1,4 +1,6 @@
+import getProduct from "@/actions/getProduct";
 import getProducts from "@/actions/getProducts";
+import Container from "@/components/ui/container";
 import { FC } from "react";
 
 interface ProductPageProps {
@@ -8,12 +10,24 @@ interface ProductPageProps {
 }
 
 const ProductPage: FC<ProductPageProps> = async ({ params }) => {
-  const product = {};
-  const suggestedProducts = await getProducts({
-    categoryId: product?.category?.id,
-  });
+  const product = await getProduct(params.productId);
 
-  return <div>ProductPage</div>;
+  return (
+    <div className="bg-white">
+      <Container>
+        <div className="px-4 py-10 sm:px-6 lg:px-8">
+          <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+            {/* Gallery */}
+            <div>Gallery</div>
+            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:my-0">
+              {/* Info */}
+              Info
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default ProductPage;
