4a57a5521bf920ba1e7a55016c826bdd71814eea
AliSabet81 <sabet.farshid1381@gmail.com>
2024-06-12 12:09:12 +0330

    no results component

--- components/productList.tsx
+++ components/productList.tsx
@@ -1,5 +1,6 @@
 import { Product } from "@/types";
 import React, { FC } from "react";
+import NoResults from "./ui/noResults";
 
 interface PropductListProps {
   title: string;
@@ -10,6 +11,7 @@ const ProductList: FC<PropductListProps> = ({ items, title }) => {
   return (
     <div className="space-y-4">
       <h3 className="font-bold text-3xl">{title}</h3>
+      {items.length === 0 && <NoResults />}
     </div>
   );
 };
