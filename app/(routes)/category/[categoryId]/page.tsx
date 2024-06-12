668ff2db55a85b0187945f04318c40809fb382e1
AliSabet81 <sabet.farshid1381@gmail.com>
2024-06-12 23:24:44 +0330

    starting category page

---
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -0,0 +1,27 @@
+import getProducts from "@/actions/getProducts";
+import { FC } from "react";
+
+interface CategoryPageProps {
+  params: {
+    categoryId: string;
+  };
+  searchParams: {
+    colorId: string;
+    sizeId: string;
+  };
+}
+
+const CategoryPage: FC<CategoryPageProps> = async ({
+  params,
+  searchParams,
+}) => {
+  const products = await getProducts({
+    categoryId: params.categoryId,
+    colorId: searchParams.colorId,
+    sizeId: searchParams.sizeId,
+  });
+
+  return <div className="bg-white"></div>;
+};
+
+export default CategoryPage;
