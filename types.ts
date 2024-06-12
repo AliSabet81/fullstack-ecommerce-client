6d3fa6093590575201c03cf625498a8393d3e32a
AliSabet81 <sabet.farshid1381@gmail.com>
2024-06-12 11:57:26 +0330

    Product types

--- types.ts
+++ types.ts
@@ -9,3 +9,31 @@ export interface Category {
   name: string;
   billboard: Billboard;
 }
+
+export interface Product {
+  id: string;
+  category: Category;
+  name: string;
+  price: string;
+  isFeatured: boolean;
+  size: Size;
+  color: Color;
+  images: Image[];
+}
+
+export interface Image {
+  id: string;
+  url: string;
+}
+
+export interface Size {
+  id: string;
+  name: string;
+  value: string;
+}
+
+export interface Color {
+  id: string;
+  name: string;
+  value: string;
+}
