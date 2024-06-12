cbdf01266c5e046a98e1d27619009c38d385cda5
AliSabet81 <sabet.farshid1381@gmail.com>
2024-06-12 11:54:37 +0330

    getbillboard data

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,14 +1,17 @@
+import getBillboard from "@/actions/getBillboard";
 import Billboard from "@/components/billboard";
 import Container from "@/components/ui/container";
 import React from "react";
 
 export const revalidate = 0;
 
-const HomePage = () => {
+const HomePage = async () => {
+  const billboard = await getBillboard("d89eb6d4-d6e3-4835-8bcf-3d670b000fb1");
+
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard data={{}} />
+        <Billboard data={billboard} />
       </div>
     </Container>
   );
